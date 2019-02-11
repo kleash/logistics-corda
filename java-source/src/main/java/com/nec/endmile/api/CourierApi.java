32eefa0e41ff7b4fc0c26d3aab9f5cca9c5f5981
shubham aggarwal <shubham6300@gmail.com>
2019-02-11 22:47:40 +0530

    api added

--- java-source/src/main/java/com/nec/endmile/api/CourierApi.java
+++ java-source/src/main/java/com/nec/endmile/api/CourierApi.java
@@ -3,12 +3,17 @@ package com.nec.endmile.api;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.nec.endmile.flow.CourierRequestFlow;
+import com.nec.endmile.schema.CourierSchemaV1;
 import com.nec.endmile.state.CourierState;
 import net.corda.core.contracts.StateAndRef;
 import net.corda.core.identity.CordaX500Name;
 import net.corda.core.identity.Party;
 import net.corda.core.messaging.CordaRPCOps;
 import net.corda.core.node.NodeInfo;
+import net.corda.core.node.services.Vault;
+import net.corda.core.node.services.vault.Builder;
+import net.corda.core.node.services.vault.CriteriaExpression;
+import net.corda.core.node.services.vault.QueryCriteria;
 import net.corda.core.transactions.SignedTransaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
@@ -24,7 +30,7 @@ import static java.util.stream.Collectors.toList;
 import static javax.ws.rs.core.Response.Status.*;
 
 // This API is accessible from /api/example. All paths specified below are relative to it.
-@Path("courier")
+@Path("couriers")
 public class CourierApi {
     private final CordaRPCOps rpcOps;
     private final CordaX500Name myLegalName;
@@ -68,9 +74,9 @@ public class CourierApi {
      * Displays all Courier states that exist in the node's vault.
      */
     @GET
-    @Path("couriers")
+    @Path("get")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<StateAndRef<CourierState>> getIOUs() {
+    public List<StateAndRef<CourierState>> getCouriers() {
         return rpcOps.vaultQuery(CourierState.class).getStates();
     }
 
@@ -80,10 +86,10 @@ public class CourierApi {
      * <p>
      * The flow is invoked asynchronously. It returns a future when the flow's call() method returns.
      *
-     * curl -X PUT 'http://localhost:10009/api/courier/create-courier?courierLength=10&courierWidth=10&courierHeight=10&courierWeight=10&source=krpuram&destination=marathahalli&partyName=O=NECAuto,L=New%20York,C=US'
+     * curl -X PUT 'http://localhost:10009/api/couriers/create?courierLength=10&courierWidth=10&courierHeight=10&courierWeight=10&source=krpuram&destination=marathahalli&partyName=O=NECAuto,L=New%20York,C=US'
      */
     @PUT
-    @Path("create-courier")
+    @Path("create")
     public Response createCourier(@QueryParam("courierLength") int courierLength, @QueryParam("courierWidth") int courierWidth,
                                   @QueryParam("courierHeight") int courierHeight, @QueryParam("courierWeight") int courierWeight,
                                   @QueryParam("source") String source,
@@ -117,17 +123,17 @@ public class CourierApi {
 
     /**
      * Displays all courier states that are created by Party.
-
+    */
     @GET
-    @Path("my-courier")
+    @Path("getByState")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getMyIOUs() throws NoSuchFieldException {
-        QueryCriteria generalCriteria = new QueryCriteria.VaultQueryCriteria(Vault.StateStatus.ALL);
-        Field lender = IOUSchemaV1.PersistentIOU.class.getDeclaredField("lender");
-        CriteriaExpression lenderIndex = Builder.equal(lender, myLegalName.toString());
-        QueryCriteria lenderCriteria = new QueryCriteria.VaultCustomQueryCriteria(lenderIndex);
-        QueryCriteria criteria = generalCriteria.and(lenderCriteria);
-        List<StateAndRef<IOUState>> results = rpcOps.vaultQueryByCriteria(criteria, IOUState.class).getStates();
+    public Response getCouriers(@QueryParam("state") String state) throws NoSuchFieldException {
+        QueryCriteria generalCriteria = new QueryCriteria.VaultQueryCriteria(Vault.StateStatus.UNCONSUMED);
+        Field status = CourierSchemaV1.PersistentCourier.class.getDeclaredField("status");
+        CriteriaExpression statusIndex = Builder.equal(status, state);
+        QueryCriteria statusCriteria = new QueryCriteria.VaultCustomQueryCriteria(statusIndex);
+        QueryCriteria criteria = generalCriteria.and(statusCriteria);
+        List<StateAndRef<CourierState>> results = rpcOps.vaultQueryByCriteria(criteria, CourierState.class).getStates();
         return Response.status(OK).entity(results).build();
-    } */
+    }
 }
